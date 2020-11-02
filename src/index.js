37ed31ed78ca62cf049d884a410122b2a352d0fd
Oksana-Hubenko89 <65037336+Oksana-Hubenko89@users.noreply.github.com>
2020-11-02 14:18:45 +0200

    убрано лишний код

--- src/index.js
+++ src/index.js
@@ -1,103 +1,40 @@
-// import menuFood from './menu.json';
-// import templateMenu from './templates/menu.hbs';
-// import './styles.css';
+import menuFood from './menu.json';
+import templateMenu from './templates/menu-card.hbs';
+import './styles.css';
 
-// const refs = {
-//   menuList: document.querySelector('.js-menu'),
-//   checkbox: document.querySelector('#theme-switch-toggle'),
-//   body: document.querySelector('body'),
-// };
-// const Theme = {
-//   LIGHT: 'light-theme',
-//   DARK: 'dark-theme',
-// };
-
-// refs.menuList.insertAdjacentHTML('afterbegin', onMenuEl(menuFood));
-
-// function onMenuEl(menuFood) {
-//   return templateMenu(menuFood);
-// }
-
-
-
-// refs.checkbox.addEventListener('change', onChangeTheme);
-
-// function onChangeTheme (evt) {
-//   if (evt.target.checked) {
-//     refs.body.classList.add(Theme.DARK);
-//     refs.body.classList.remove(Theme.LIGHT);
-//     localStorage.setItem('theme','dark');
-//   } else {        
-//     refs.body.classList.add(Theme.LIGHT);
-//     refs.body.classList.remove(Theme.DARK);
-//     localStorage.setItem('theme','light');
-        
-//   }
-  
-// }
-
-// document.addEventListener('DOMcontentLoaded', onSaveTheme);
-
-// function onSaveTheme () {
-//   if (localStorage.getItem('theme') === 'dark') {
-//     refs.body.classList.add(Theme.DARK);
-//     refs.checkbox.checked = true;
-//   }
-//   else {
-//     refs.body.classList.add(Theme.LIGHT);
-//     refs.checkbox.checked = false;
-//   }
-// }
-
-// // const themeChosen = storage.load('Theme');
-// // refs.checkbox.checked = themeChosen == 'Theme.DARK' ? true : false;
-// // refs.body.classList.add(themeChosen ? themeChosen : Theme.LIGHT);
-
-// // function onChangeTheme() {
-// //     refs.body.classList.toggle(Theme.DARK, !refs.body.classListtoggle(Theme.LIGHT),
-// //     storage.save('Theme', refs.body.classList[0]));
-// // };
-
-// // function populateTheme() {
-// //   const savedTheme = localStorage.getItem(STORAGE_KEY_TEME);
-// //   if (savedTheme) {
-// //     refs.checkbox.value = savedTheme;
-// //   }
-// //   }
-import menuCard from '../src/templates/menu-card.hbs';
-import menu from './menu.json';
-import './styles.css'; 
-
+const refs = {
+  menuList: document.querySelector('.js-menu'),
+  checkbox: document.querySelector('#theme-switch-toggle'),
+  body: document.querySelector('body'),
+  savedTheme: localStorage.getItem('Theme'),
+};
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
-const body = document.body;
 
-const menuContainer = document.querySelector('.js-menu');
+refs.menuList.insertAdjacentHTML('afterbegin', onMenuEl(menuFood));
 
-const menuCards = menuCard(menu);
-
-
-const themeChanger = document.querySelector('#theme-switch-toggle');
-const savedTheme = localStorage.getItem('Theme');
-
-themeChanger.addEventListener('change', themeSwitch);
-menuContainer.insertAdjacentHTML('beforeend', menuCards);
-
-body.classList.add(savedTheme);
-
-if (savedTheme === Theme.DARK) { 
-    themeChanger.checked = true;
+function onMenuEl(menuFood) {
+  return templateMenu(menuFood);
 }
 
-function themeSwitch(e) {
+refs.checkbox.addEventListener('change', onChangeTheme);
+
+refs.body.classList.add(refs.savedTheme);
+
+if (refs.savedTheme === Theme.DARK) { 
+    refs.checkbox.checked = true;
+}
+
+function onChangeTheme(e) {
 if (e.target.checked) {
-body.classList.remove(Theme.LIGHT);
-body.classList.add(Theme.DARK);
+refs.body.classList.remove(Theme.LIGHT);
+refs.body.classList.add(Theme.DARK);
 } else {
-body.classList.remove(Theme.DARK);
-body.classList.add(Theme.LIGHT);
+refs.body.classList.remove(Theme.DARK);
+refs.body.classList.add(Theme.LIGHT);
 }
-localStorage.setItem('Theme', body.classList);
+  localStorage.setItem('Theme', refs.body.classList);
 }
+
